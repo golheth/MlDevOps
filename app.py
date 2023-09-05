dbb95b44b3eccc2e60ec076d15c6aa7f75e76033
golheth <87906503+golheth@users.noreply.github.com>
2023-09-05 15:00:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import uvicorn
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from typing import List
-from pydantic import BaseModel
+
 
 from fastapi import FastAPI
 app = FastAPI()
@@ -10,7 +10,7 @@ app = FastAPI()
 
 
 # Importa tus funciones y DataFrames
-from Functions import userdata, countreviews, genre_rank
+from Functions import get_game_recommendations
 
 
 
@@ -18,18 +18,10 @@ from Functions import userdata, countreviews, genre_rank
 class GameRequest(BaseModel):
     game_name: str
 
-class UserRequest(BaseModel):
-    user_id: str
-
-class DateRangeRequest(BaseModel):
-    date1: str
-    date2: str
-
 
 
 # Carga tus DataFrames
-df_games_items2 = pd.read_parquet('df_games_items2.parquet')
-df_games_reviews2 = pd.read_parquet('df_games_reviews2.parquet')
+
 df_combined = pd.read_parquet('df_combined.parquet')
 
 
@@ -39,17 +31,7 @@ df_combined = df_combined.reset_index(drop=True)
 def read_root():
     return {"message": "Bienvenido a tu API personalizada"}
 
-@app.post("/userdata/")
-def get_user_data(user_request: UserRequest):
-    money_spent, recommend_percentage, num_items = userdata(
-        user_request.user_id, df_games_reviews2, df_games_items2, df_combined)
-    return {"money_spent": money_spent, "recommend_percentage": recommend_percentage, "num_items": num_items}
 
-@app.post("/countreviews/")
-def count_reviews(date_range_request: DateRangeRequest):
-    num_users, percentage_recommendations = countreviews(
-        date_range_request.date1, df_games_reviews2, date_range_request.date2)
-    return {"num_users": num_users, "percentage_recommendations": percentage_recommendations}
 
 
 @app.post("/recommendations/")
